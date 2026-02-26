644491b83dbddc8de05f4866334d28c2628e01eb
YoungJoon Jeong <yjeong@amazon.com>
2026-02-26 16:18:58 +0900

    fix: address PR review comments - remove redundant terraform changes
    - Remove cluster_encryption_config (EKS module handles this by default)
    - Remove explicit cloudwatch_log_group_name (redundant with module default)
    - Remove comment-only change in eks-addons.tf
    - Delete MULTI_CLUSTER_SUPPORT.md per reviewer request

--- terraform/modules/eks-auto-mode/eks.tf
+++ terraform/modules/eks-auto-mode/eks.tf
@@ -38,14 +38,6 @@ module "eks" {
     ECRPullThroughCache = aws_iam_policy.ecr_pull_through_cache.arn
   }
 
-  # Use cluster name in KMS alias to avoid conflicts
-  cluster_encryption_config = {
-    resources        = ["secrets"]
-    provider_key_arn = null
-  }
-
-  # Use cluster name in CloudWatch log group to avoid conflicts
-  cloudwatch_log_group_name = "/aws/eks/${var.name}/cluster"
 }
 
 resource "null_resource" "update_kubeconfig" {
